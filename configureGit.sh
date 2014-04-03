116c04bdfc998f4384c9634ec345fadb097b132a
Bryce Parry <bwparry@calpoly.edu>
2014-04-03 13:04:55 -0700

    added a note to tell me what I did

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script configures git whenever you start working 
+# on multiple machines. Run it anytime you decide to work with
+# git on a new computer.
+
 git config --global user.name "Bryce Parry"
 git config --global user.email "bwparry@calpoly.edu"
 git config --global color.ui "auto"
