58a489d7fe2882889e1ded68c6c0f8a6a1da768a
Bryce Parry <bwparry@calpoly.edu>
2014-04-03 12:54:43 -0700

    adding config and pledge files

---
+++ configureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "Bryce Parry"
+git config --global user.email "bwparry@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
+
